ac77d65d7618c8aa5ea7d1df75b612d0c4b25e0e
awest <west.andrew.g@gmail.com>
2016-10-04 22:56:49 -0400

    Minor IRC changes

---
+++ irc_work/irc_echo_test.java
@@ -0,0 +1,60 @@
+package irc_work;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.schwering.irc.lib.IRCConnection;
+
+/**
+ * Andrew G. West - irc_echo_test.java - Simple echo listener on an IRC
+ * feed in order to debug connections and output. 
+ */
+public class irc_echo_test extends Thread{
+
+	// ***************************** MAIN METHOD ****************************
+		
+	/**
+	 * Test harness
+	 * @param args No arguments are taken by this method
+	 */
+	public static void main(String[] args) throws Exception{
+		ExecutorService pool = Executors.newFixedThreadPool(4);
+		new irc_echo_test(pool);
+		Thread.sleep(50000);
+	}	
+	
+		
+	// ***************************** CONSTRUCTORS ****************************	
+	
+	/**
+	 * Construct a [irc_echo_test] class, connecting to the en.wikipedia
+	 * recent changes feed and mirroring all output to STDOUT. This 
+	 * is envisioned primarily as a debugging tool.
+	 */
+	public irc_echo_test(ExecutorService threads){
+		
+		String host = "irc.wikimedia.org"; // irc.cluenet.org
+		int p_min = 6667; // Port-range minimum
+		int p_max = 6669; // Port-range maximum
+		String pass = null;
+		String user = "STikiQueuer2";
+
+			// Prepare connection, event-handler, and basic-settings
+		IRCConnection con_irc;
+		con_irc = new IRCConnection(host, p_min, p_max, pass, user, user, user);
+		con_irc.addIRCEventListener(new irc_echo_events()); 
+		con_irc.setDaemon(true);
+		con_irc.setColors(false); 
+
+		try{ con_irc.connect(); } catch(IOException ioexc){
+			System.out.println("Error establishing RC IRC connection:");
+			ioexc.printStackTrace(); 
+		} // Establish connection to IRC server
+		
+			// With connection established, join channel, fork thread
+		con_irc.doJoin("#en.wikipedia"); // #cluebotng-spam		
+		start(); // Start as own thread, so processing can occur
+	}
+
+}
